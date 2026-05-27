5f898049b06601353b0864f9e2c6a2f66ba01fca
unknown <achillesaeon8-svg>
2026-05-27 13:02:11 +0800

    Debug of variable and indention

--- main.py
+++ main.py
@@ -2,8 +2,15 @@ import pandas as pad
 import numpy as nup
 
 def main():
+
     try:
         np_list = pad.read_csv('nobel.csv')
     except FileNotFoundError:
         print('Error: "nobel.csv" not found in your current folder directory')
         print('Please ensure "noble.csv" is placed in the exact same folder as the program for it to be identified and successfuly run.')
+        return
+    
+# QUESTION NO.1: MOST AWARDED GENDER AND BIRTH COUNTRY
+
+    gender_counts = np_list['sex'].value_counts()
+    country_counts = np_list['birth_country']
